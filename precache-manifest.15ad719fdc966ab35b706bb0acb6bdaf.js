058b1f588afecbd1729860f82ea50b9eade1dcd6
Roger <thrasher_royerr666@hotmail.com>
2020-04-27 16:36:51 -0500

    Updates

---
+++ precache-manifest.15ad719fdc966ab35b706bb0acb6bdaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd5e6d269dc76c3bf2d325d48c4f7ce5",
+    "url": "/CharDeleter/index.html"
+  },
+  {
+    "revision": "d91eda6db5c987b7934f",
+    "url": "/CharDeleter/static/css/main.a853fc59.chunk.css"
+  },
+  {
+    "revision": "fb95f7484680b5db3d6a",
+    "url": "/CharDeleter/static/js/2.ee512048.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CharDeleter/static/js/2.ee512048.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d91eda6db5c987b7934f",
+    "url": "/CharDeleter/static/js/main.43c91f6d.chunk.js"
+  },
+  {
+    "revision": "9055448af63bde95c2ae",
+    "url": "/CharDeleter/static/js/runtime-main.d50e2057.js"
+  }
+]);
